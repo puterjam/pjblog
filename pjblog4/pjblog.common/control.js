269077f32137fe9e9caeff06597ad3b4af0108b6
levevio@gmail.com <levevio@gmail.com@8c33ed6e-f04b-0410-a338-67e2075e6109>
2009-12-28 09:11:18 +0000

    新界面

---
+++ pjblog4/pjblog.common/control.js
@@ -0,0 +1,102 @@
+﻿// JavaScript Document
+$(function(){
+	$('a, input[type="button"]').bind('focus',function(){
+		if(this.blur){ //如果支持 this.blur
+			this.blur();
+		}
+	});
+	var timer;
+	try{
+		for (var j = 0; j < 2 ; j++){
+			$(".lay_" + j).hover(
+				function(){
+					clearTimeout(timer);
+					$('#overT').eBoxClose();
+					var ii = $(this).attr("class");
+					var iii = parseInt(ii.replace("lay_", ""));
+					$.eBoxClick(this, {
+					  css : {
+						  width : "172px",
+						  height : "60px",
+						  background : "url(../../images/Control/overlay_1.png) no-repeat"
+					  },
+					  n 	: 	10,
+					  html 	: 	ReadDom(iii),
+					  fix 	: 	false,
+					  id 	: 	"overT",
+					  oft 	: 	0,
+					  ofl 	: 	0
+				  });
+				},
+				function(){
+					timer = setTimeout("$('#overT').eBoxClose()", 500);
+				}
+			);
+		}
+	}catch(e){}
+	
+	function ReadDom(j){
+		return "<div class=\"ovlay-content\"><div class=\"con\">" + $("#ovlayinfo #lay_" + j).text() + "</div></div>";
+	}
+});
+
+var conMain = {}
+
+conMain.seesion = "";
+
+conMain.tip = {
+	edit : function(){
+		conMain.seesion = $(".te").html();
+		var word = $(".word").text();
+		var c = "<form action=\"../pjblog.logic/control/log_general.asp?action=SaveTipWord\" method=\"post\" id=\"post_tip\">"
+					+ "<input type=\"text\" value=\"" + word + "\" class=\"text\" style=\"width:235px;\" name=\"tip_word\" /><br />"
+					+ "<a href=\"javascript:;\" onclick=\"conMain.tip.post()\">保存</a>&nbsp;&nbsp;<a href=\"javascript:;\" onclick=\"conMain.tip.close()\">取消</a>&nbsp;&nbsp;<span id=\"tip01\"></span>"
+					+ "</form>";
+		$(".te").html(c);
+	},
+	close : function(){
+		$(".te").html(conMain.seesion);
+	},
+	post : function(){
+		var c = $("input[name='tip_word']").val();
+		$("#post_tip").ajaxSubmit({
+			dataType : "json",
+			beforeSubmit : function(){
+				$("#tip01").text("wait, posting..").css({
+					"font-style" : "oblique",
+					"font-size" : "10px",
+					"line-height" : "20px",
+					color : "#ff0000"
+				});
+			},
+			success : function(data){
+				if (data.Suc){
+					conMain.tip.close();
+					$(".word").text(c);
+				}else{
+					jAlert(data.Info, "错误信息");
+					$("#tip01").text("")
+				}
+			}			   
+		});
+	}
+}
+
+conMain.PersonSet = function(obj){
+	try{$('#PersonSet').eBoxClose();}catch(e){}
+	var c = "<div class=\"person_top\" onclick=\"$('#PersonSet').eBoxClose()\"></div>"
+			+ "<div class=\"person_mid\"><div class=\"cContent\"><ul><li><a href=\"\">修改我的基本资料</a></li><li><a href=\"\">修改整站基本信息</a></li><li><a href=\"\">编辑网站基本权限</a></li></ul></div></div>"
+			+ "<div class=\"person_bom\"></div>";
+	$.eBoxClick(obj, {
+		css : {
+			background : "#ffffff",
+			width : "176px"
+		},
+		n 		: 	11,
+		html 	: 	c,
+		fix 	: 	false,
+		id 		: 	"PersonSet",
+		oft 	: 	0,
+		ofl 	: 	0
+	});
+}
---
+++ pjblog4/pjblog.common/jquery/jquery.ui.ebox.js
@@ -0,0 +1,177 @@
+﻿// JavaScript Document
+/*
+$.eBoxClick(obj, {
+	css : {
+		background : "#ff0000",
+		width:"100px",
+		height:"100px",
+		"z-index" : 99
+	},
+	html:"evio",
+	fix : true,
+	-------------- clear -------------
+	n : 0,
+	ofl : 0,
+	oft : 0
+});
+*/	
+	
+;(function($){
+	$.eBoxClick = function(obj, options){
+
+		var div = document.createElement("div");
+		$("body").append(div);
+		
+		options = $.extend({
+			id		: 	null,
+			n		: 	null,
+			ofl 	: 	null,
+			oft 	: 	null,
+			css 	: 	null,
+			html 	: 	null,
+			fix 	: 	true
+		}, options || {});
+		
+		$(div).html(options.html).css(options.css);
+
+		if (options.id != null) $(div).attr("id", options.id);
+		
+		var off = $(obj).offset();
+		
+		var c = {
+			n 	:	options.n,
+			ol 	: 	off.left,
+			ot 	: 	off.top,
+			ow 	: 	$(obj).outerWidth(),
+			oh 	: 	$(obj).outerHeight(),
+			dw 	: 	$(div).outerWidth(),
+			dh 	: 	$(div).outerHeight(),
+			ofl : 	options.ofl,
+			oft : 	options.oft
+		}
+
+		var pos = getPostion(c);
+
+		var left = pos.left;
+		var top = pos.top;
+
+		$(div).css({
+			left : left + "px",
+			top : top + "px"
+		});
+		
+		if (options.fix){
+			$(div).css("position", "fixed");
+		}else{
+			$(div).css("position", "absolute");
+		}
+		//$(div).fadeOut(0).fadeIn(600);
+	}
+/*
+		option : options
+		{
+			n :  0 - 16,
+			ol : object left,
+			ot : object top,
+			ow : object width,
+			oh : object height,
+			dw : div width,
+			dh : div height,
+			ofl : div offset left,
+			oft : div offset top
+		}
+*/
+	function getPostion(option){
+		var left, top;
+		var n 	= 	option.n;
+		var ol 	= 	option.ol;
+		var ot 	= 	option.ot;
+		var ow 	= 	option.ow;
+		var oh 	= 	option.oh;
+		var dw 	= 	option.dw;
+		var dh 	= 	option.dh;
+		var ofl = 	option.ofl;
+		var oft = 	option.oft;
+		if (ofl == null) ofl = 0;
+		if (oft == null) oft = 0;
+		switch (n){
+			case 0 :
+				left = ol + ofl;
+				top = ot + oft;
+				break;
+			case 1 :
+				left = ol + ofl;
+				top = ot - dh + oft;
+				break;
+			case 2 :
+				left = ol + parseInt((ow - dw) / 2) + ofl;
+				top = ot - dh + oft;
+				break;
+			case 3 :
+				left = ol + (ow - dw) + ofl;
+				top = ot - dh + oft;
+				break;
+			case 4 :
+				left = ol + ow + ofl;
+				top = ot - dh + oft;
+				break;
+			case 5 : 
+				left = ol + ow + ofl;
+				top = ot + oft;
+				break;
+			case 6 :
+				left = ol + ow + ofl;
+				top = ot + parseInt((oh - dh) / 2) + oft;
+				break;
+			case 7 :
+				left = ol + ow + ofl;
+				top = ot + (oh - dh) + oft;
+				break;
+			case 8 :
+				left = ol + ow + ofl;
+				top = ot + oh + oft;
+				break;
+			case 9 :
+				left = ol + (ow - dw) + ofl;
+				top = ot + oh + oft;
+				break;
+			case 10 :
+				left = ol + parseInt((ow - dw) / 2) + ofl;
+				top = ot + oh + oft;
+				break;
+			case 11 :
+				left = ol + ofl;
+				top = ot + oh + oft;
+				break;
+			case 12 :
+				left = ol - dw + ofl;
+				top = ot + oh + oft;
+				break;
+			case 13 :
+				left = ol - dw + ofl;
+				top = ot + (oh - dw) + oft;
+				break;
+			case 14 :
+				left = ol - dw + ofl;
+				top = ot + parseInt((oh - dw) / 2) + oft;
+				break;
+			case 15 :
+			 	left = ol - dw + ofl;
+				top = ot + oft;
+				break;
+			case 16 :
+				left = ol - dw + ofl;
+				top = ot - dh + oft;
+				break;
+			default : 
+				left = parseInt(parseInt($(window).width()/ 2) - parseInt(dw / 2));
+				top = parseInt(parseInt($(window).height()/ 2) - parseInt(dh / 2));
+		}
+		
+		return {left : left, top : top};
+	}
+	
+	$.fn.eBoxClose = function(){
+		this.remove();
+	}
+})(jQuery);
